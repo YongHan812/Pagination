883bfd39c6311a60d551d23f5bb0ba89e2dd9186
yonghan <yonghanfoon@gmail.com>
2022-02-23 09:33:46 +0800

    Initial Commit

---
+++ Pagination/Models/DemoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pagination.Models
+{
+    public class DemoViewModel
+    {
+        public string IdSortParam { get; set; }
+        public string NameSortParam { get; set; }
+        public string SearchName { get; set; }
+        public List<DemoTable> Demos { get; set; }
+        public int PageIndex { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
